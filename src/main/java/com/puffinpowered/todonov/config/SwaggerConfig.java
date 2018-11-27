edeecd1d0ea0cf09ed78d32d8873cdfaedf5e92f
pws-nla <pshields@nla.gov.au>
2018-11-27 12:01:36 +1100

    Configure swagger api to only show required Error response codes

--- src/main/java/com/puffinpowered/todonov/config/SwaggerConfig.java
+++ src/main/java/com/puffinpowered/todonov/config/SwaggerConfig.java
@@ -2,10 +2,14 @@ package com.puffinpowered.todonov.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RequestMethod;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseMessageBuilder;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+import static com.google.common.collect.Lists.newArrayList;
 
 @Configuration
 @EnableSwagger2
@@ -15,7 +19,19 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.puffinpowered.todonov"))
-                .build();
+                .build()
+                .useDefaultResponseMessages(false)
+                .globalResponseMessage(RequestMethod.GET, newArrayList(new ResponseMessageBuilder().code(404)
+                                .message("Item does not exist")
+                                .build()))
+                .globalResponseMessage(RequestMethod.PUT, newArrayList(new ResponseMessageBuilder().code(404)
+                        .message("Item does not exist")
+                        .build()))
+                .globalResponseMessage(RequestMethod.DELETE, newArrayList(new ResponseMessageBuilder().code(404)
+                        .message("Item does not exist")
+                        .build()))
+                ;
+
     }
 
 }
